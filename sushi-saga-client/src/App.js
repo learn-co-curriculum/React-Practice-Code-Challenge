14153bf563c7eb540927a10f01ea091972bf434e
SofiaFredriksson <sofiapkf@gmail.com>
2019-11-03 22:35:35 +0000

    destructured done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,65 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    startIndex: 0,
+    eatenSushis: [],
+    budget: 100,
+    noBudget: false,
+  }
+
+
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushis => this.setState({sushis}))
+  }
+
+  nextFour = () => {
+    if (this.state.startIndex + 4 >= this.state.sushis.length) {
+    this.setState({
+      startIndex: this.state.startIndex + 4 - this.state.sushis.length
+    })
+  } else {
+    this.setState({
+      startIndex: this.state.startIndex + 4
+    })
+  }
+  }
+
+  beenEaten = (id, price) => {
+    if (this.state.eatenSushis.includes(id)) return;
+    if (this.state.budget < price) {
+      this.setState({
+        noBudget: true
+      })
+    } else {
+      this.setState({
+        eatenSushis: [...this.state.eatenSushis, id],
+        budget: this.state.budget - price,
+        noBudget: false
+      })
+    }
+  
+  }
+
+
+
+
   render() {
+    const {startIndex, eatenSushis, budget, noBudget} = this.state
+
+    const finalSushis = this.state.sushis.map(sushi => ({...sushi, eaten: this.state.eatenSushis.includes(sushi.id)}))
+    const fourSushis = finalSushis.slice(startIndex, startIndex + 4)
+
+    console.log(startIndex)
+
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer  beenEaten={this.beenEaten} nextFour={this.nextFour} sushis={fourSushis}/>
+        <Table noBudgetMsg={noBudget} eatenSushis={eatenSushis} budget={budget}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
-const Sushi = (props) => {
+const Sushi = ({sushi, beenEaten}) => {
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={() => beenEaten(sushi.id, sushi.price)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
-          true ?
+          sushi.eaten ?
             null
           :
-            <img src={/* Give me an image source! */} width="100%" />
+            <img alt={sushi.id} src={sushi.img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {sushi.name} - ${sushi.price}
       </h4>
     </div>
-  )
+  ) 
 }
 
 export default Sushi
